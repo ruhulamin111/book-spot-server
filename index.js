f50a99b299a79b6597dfccda59931636d1839ef1
ruhulamin111 <ruhuljuwel111@gmail.com>
2022-05-18 22:55:45 +0600

    design update

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+
 app.use(cors());
 app.use(express.json())
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqw9k.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-
     try {
         await client.connect();
         const itemCollection = client.db("bookSpot").collection("item");
         console.log('connected db');
+
 
         app.get('/item', async (req, res) => {
             const query = {}
@@ -25,6 +27,7 @@ async function run() {
             res.send(books);
         });
 
+
         app.get('/item/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
@@ -32,11 +35,13 @@ async function run() {
             res.send(book)
         });
 
+
         app.post('/item', async (req, res) => {
             const newItem = req.body;
             const result = await itemCollection.insertOne(newItem);
             res.send(result);
         })
+
 
         app.delete('/item/:id', async (req, res) => {
             const id = req.params.id;
@@ -44,19 +49,18 @@ async function run() {
             const result = await itemCollection.deleteOne(query);
             res.send(result);
         })
-
-
     }
     finally {
 
     }
-
 }
 run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('The Book Spot Server is Running');
 });
+
 
 app.listen(port, () => {
     console.log('Server is Running');
